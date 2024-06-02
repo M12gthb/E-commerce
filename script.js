4b5322c8b7ca08f3caaa360900b21668112d9a2b
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-06-02 01:29:16 -0300

    feat: cart

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // Requisição
+async function main() {
+    try {
+        products = await requestCompanies();
+        renderCards(products,0)
+        setupPagination(products)
+        setUpSearch(products)
+        renderCart()
+        footerButton()
+    } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+    }
+}
 
 async function requestCompanies() {
     try {
@@ -18,42 +30,16 @@ async function requestCompanies() {
     }
 }
 
-async function main() {
-    try {
-        let products = await requestCompanies();
-        renderCards(products,0)
-        setupPagination(products)
-        setUpSearch(products)
-        // setFilterSearch(products)
-    } catch (error) {
-        console.error('Erro ao buscar produtos:', error);
-    }
-}
-
-// Variáveis
-let cont = 0;
-let soma = 0;
-let index = 0;
-let cart = new Map;
-
-const footerButton = document.querySelector(".footer-button");
-const search = document.querySelector(".searchBar");
-const select = document.querySelector(".select");
-const cartButton = document.querySelector(".cart-Button");
-const cartModal = document.querySelector(".cart");
-const cardModal = document.querySelector(".details");
-const nextPage = document.querySelector(".next")
-const previusPage = document.querySelector(".previous")
-const paginationContainer = document.querySelector(".pagination-container")
-
-
 // Lógica do retorno do footer
-footerButton.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
-})
+function footerButton() {
+    const footerButton = document.querySelector(".footer-button");
+    footerButton.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    })
+}
 
 // Renderizar os posts
 function renderCards(products, pageIndex) {
@@ -64,6 +50,9 @@ function renderCards(products, pageIndex) {
     const startIndex = pageIndex * itemsPerPage;
     const endIndex = Math.min(startIndex + itemsPerPage, products.length);
     const pageItems = products.slice(startIndex, endIndex);
+    const nextPage = document.querySelector(".next")
+    const previusPage = document.querySelector(".previous")
+    const cardModal = document.querySelector(".details");
     
     if (pageIndex >= totalPages) {
         pageIndex = totalPages -1;
@@ -71,7 +60,6 @@ function renderCards(products, pageIndex) {
     if (pageIndex < 0) {
         pageIndex = 0;
     }
-    
     
     if (totalPages > 1) {
         paginationText.innerText = ` ${pageIndex + 1} de ${totalPages}`;
@@ -138,48 +126,65 @@ function renderCards(products, pageIndex) {
         card.addEventListener("click", () => {
         const closeButton = document.querySelector(".details-closeButton")
         cardModal.showModal()
-       
+        console.log(card.id)
         closeButton.addEventListener("click", () => {
             cardModal.close()
         })
         cardModal.addEventListener('click', (event) => {
-        if (event.target === cartModal) {
-                cartModal.close();
+        if (event.target === cardModal) {
+                cardModal.close();
             }
         })
     }))
+    const addButton = document.querySelectorAll(".add")
     
+    addButton.forEach(button => {
+        button.addEventListener("click", () => {
+            const findProduct = products.find(element => element.id == button.id);
+            if (findProduct) {
+                if (cart.has(findProduct.id)) {
+                    const currentItem = cart.get(findProduct.id);
+                    currentItem.count += 1;
+                    cart.set(findProduct.id, currentItem);
+                    renderCart(cart)
+                } else {
+                    cart.set(findProduct.id, { findProduct: findProduct, count: 1 });
+                }
+            }
+            renderCart(cart)
+        });
+    });
     
 }
 
 // Paginação
 function setupPagination(products) {
     const totalPages = Math.ceil(products.length / 10); 
+    const nextPage = document.querySelector(".next")
+    const previusPage = document.querySelector(".previous")
     let index = 0;
-
+    
     const updatePage = (newIndex) => {
         index = (newIndex + totalPages) % totalPages;
         renderCards(products, index);
     };
-
+    
     nextPage.addEventListener("click", () => {
         updatePage(index + 1);
     });
-
+    
     previusPage.addEventListener("click", () => {
         updatePage((index - 1 + totalPages) % totalPages);
     });
-
-    // Renderizar a primeira página
+    
     renderCards(products, index);
 }
-
-
-
 
 // Barra de pesquisa
 function setUpSearch(products){
     const allProducts = products
+    const search = document.querySelector(".searchBar");
+    const select = document.querySelector(".select");
     search.addEventListener("input", (e) => {
         if(e.target.value == ""){
             renderCards(allProducts,0)
@@ -190,7 +195,7 @@ function setUpSearch(products){
     })
     select.addEventListener("change", (e) => {
         if(e.target.value == ""){
-         renderCards(allProducts,0)
+            renderCards(allProducts,0)
         }
         const filteredProductsbyType = allProducts.filter((element) => element.type.toLowerCase().includes(e.target.value.toLowerCase()))
         renderCards(filteredProductsbyType, 0)
@@ -198,16 +203,114 @@ function setUpSearch(products){
     })
 }
 
-// Abrir e fechar o carrinho
+// Abrir e fechar o carrinho e Renderizar o carrinho
+function renderCart() {
+    let cart = new Map;
+    const ul = document.querySelector(".cart-ul");
+    const cartButton = document.querySelector(".cart-Button");
+    const cartModal = document.querySelector(".cart");
+    const closeButton = document.querySelector(".cart-closeButton");
+    const totalPrice = document.createElement("h1")
+    const totalCount = document.createElement("h1")
+    const totalItens = document.querySelector(".cartCont")
 
+    cartButton.addEventListener("click", () => {
+        cartModal.showModal();
 
-// Renderizar o carrinho
+        closeButton.addEventListener("click", () => {
+            cartModal.close();
+        });
+        cartModal.addEventListener('click', (event) => {
+            if (event.target === cartModal) {
+                cartModal.close();
+            }
+        });
+    });
 
+    ul.innerHTML = "";
+
+    if (cart.size == 0) { 
+        const title = document.createElement("h1")
+        title.innerText = "Carrinho"
+        return ul.appendChild(title)
+    }
+    
+    totalItens.innerText = `${Array.from(cart.values()).reduce((acc, item)=> {
+        return acc + item.count
+    },0)}`
+    totalPrice.innerText = `Total: R$ ${Array.from(cart.values()).reduce((acc, item)=> {
+        return acc + (item.count * item.findProduct.price)
+    },0).toFixed(2)}`
+    totalCount.innerText = `Quantidade: ${Array.from(cart.values()).reduce((acc, item)=> {
+        return acc + item.count
+    },0)}`
+    ul.append(totalPrice, totalCount)
+       
+    Array.from(cart.values()).forEach((element) => {
+        let li = document.getElementById(`cart-item-${element.findProduct.id}`);
+        
+        if (!li) {
+            li = document.createElement("li");
+            li.id = `cart-item-${element.findProduct.id}`;
+            
+            const img = document.createElement("img");
+            img.src = element.findProduct.image;
+            
+            const title = document.createElement("h3");
+            title.innerText = element.findProduct.name;
+            
+            const decrement = document.createElement("button");
+            decrement.id = element.id
+            decrement.innerText = "-";
+            
+            const count = document.createElement("p");
+            count.innerText = element.count;
+            count.className = "count";
+
+            const increment = document.createElement("button");
+            increment.id = element.id
+            increment.innerText = "+";
+            
+            const remove = document.createElement("button");
+            remove.id = element.id
+            remove.innerText = "Remove";
+            
+            decrement.addEventListener("click", () => updateCount(element.findProduct.id, -1, cart));
+            increment.addEventListener("click", () => updateCount(element.findProduct.id, 1, cart));
+            remove.addEventListener("click", () => removeItem(element.findProduct.id, cart));
+            
+            li.append(img, title, decrement, count, increment, remove);
+            ul.appendChild(li); 
+        } else {
+            li.querySelector(".count").innerText = element.count;
+        }
+    });
+}
+
+function updateCount(productId, change, cart) {
+    if (cart.has(productId)) {
+        const currentItem = cart.get(productId);
+        currentItem.count += change;
+        
+        if (currentItem.count <= 0) {
+            cart.delete(productId);
+        } else {
+            cart.set(productId, currentItem);
+        }
+        renderCart(cart); 
+    }
+}
+
+function removeItem(productId, cart) {
+    cart.delete(productId);
+    renderCart(cart); 
+}
 
 // Renderizar as informações do modal de posts
 
+
 // Manter itens no carrinho
 
-// Chamado das funções
 
+// Chamado das funções
 main()
